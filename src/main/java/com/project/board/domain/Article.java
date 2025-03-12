aa49f50ba40213c8d218c3aa3ac16348b1967b60
xxo <xxoxec@gmail.com>
2025-03-12 18:33:23 +0900

    #13-AuditingFlelds 추출
    생성일자, 생성자, 수정일자, 수정자는 엔티티 클래스에 들어가는 반복 요소이고, 도메인과 직접적인 연관이 없는 요소이므로 추출이 가능
    
    `@MappedSuperclass` 이용 상속방식으로 추출
    
    `@DateTimeFormat` 요소 추가 및 jpa 옵션 개선

--- src/main/java/com/project/board/domain/Article.java
+++ src/main/java/com/project/board/domain/Article.java
@@ -24,9 +24,9 @@ import java.util.Set;
         @Index(columnList = "createBy")
 })
 
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class Article {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue
@@ -43,12 +43,10 @@ public class Article {
     @ToString.Exclude
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createAt; // 생성일자
-    @CreatedBy @Column(nullable = false, length = 100) private String createBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일자
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
-
+//    @CreatedDate @Column(nullable = false) private LocalDateTime createAt; // 생성일자
+//    @CreatedBy @Column(nullable = false, length = 100) private String createBy; // 생성자
+//    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일자
+//    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
     protected Article() {}
 
--- src/main/java/com/project/board/domain/ArticleComment.java
+++ src/main/java/com/project/board/domain/ArticleComment.java
@@ -21,9 +21,9 @@ import java.util.Objects;
         @Index(columnList = "createBy")
 })
 
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
 
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,10 +31,10 @@ public class ArticleComment {
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createAt; // 생성일자
-    @CreatedBy @Column(nullable = false, length = 100) private String createBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일자
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+//    @CreatedDate @Column(nullable = false) private LocalDateTime createAt; // 생성일자
+//    @CreatedBy @Column(nullable = false, length = 100) private String createBy; // 생성자
+//    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일자
+//    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
     protected ArticleComment() {}
 
