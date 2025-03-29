3b7e57aadddd4eb7b03f31afa91475c8c7e7a5a4
xxo <xxoxec@gmail.com>
2025-03-29 16:13:45 +0900

    #17-login controller test + security 설정을 controller test에 반영
    springsecurity가 자동 생성해준 `/login` controller를 test.
    모든 controller가 security 설정을 부를 수 있도록 `@Import(SecurityConfig.class)` 추가

--- src/test/java/com/project/board/controller/ArticleControllerTest.java
+++ src/test/java/com/project/board/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.project.board.controller;
 
+import com.project.board.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -14,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("view controller - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
