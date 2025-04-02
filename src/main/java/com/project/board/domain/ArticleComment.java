f2e5b6c6db1f293eba4997eea25ea9ad0c657724
xxo <xxoxec@gmail.com>
2025-04-02 13:36:12 +0900

    #18-리팩토링: 사용하지 않는 import 정리
    사용하지 않는 import를 정리해서 코드를 정리

--- src/main/java/com/project/board/domain/ArticleComment.java
+++ src/main/java/com/project/board/domain/ArticleComment.java
@@ -4,70 +4,38 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
 @ToString
 @Table(indexes = {
         @Index(columnList = "content"),
-        @Index(columnList = "createAt"),
-        @Index(columnList = "createBy")
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
 })
-
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
-
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Setter
-    @ManyToOne(optional = false)
-    private Article article; // 게시글 (ID)
-    @Setter
-    @Column(nullable = false, length = 500)
-    private String content; // 본문
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createAt; // 생성일자
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createBy; // 생성자
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; // 수정일자
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; // 수정자
-
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
     protected ArticleComment() {}
-
     private ArticleComment(Article article, String content) {
         this.article = article;
         this.content = content;
     }
-
     public static ArticleComment of(Article article, String content) {
         return new ArticleComment(article, content);
     }
-
     @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (!(o instanceof ArticleComment that)) return false;
         return id != null && id.equals(that.id);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return Objects.hash(id);
     }
 }
